e3b84a57591e509702fd8092cbaeedbde067628b
teja <2303A52298@sru.edu.in>
2026-04-06 11:45:14 +0530

    upgraded professional UI

--- static/script.js
+++ static/script.js
@@ -1,43 +1,54 @@
+let mainChart, metricChart;
+
 async function runTest(){
 
 let size = document.getElementById("size").value;
 
 let res = await fetch("/run",{
-
 method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify({
-size:size
-})
-
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({size:size})
 });
 
 let data = await res.json();
 
-let ctx=document.getElementById("chart");
+document.getElementById("serial").innerText = data.serial.toFixed(4);
+document.getElementById("parallel").innerText = data.parallel.toFixed(4);
+document.getElementById("distributed").innerText = data.distributed.toFixed(4);
 
-new Chart(ctx,{
+let speedup = data.serial / data.parallel;
+let efficiency = speedup / 4;
 
-type:"bar",
+document.getElementById("speedup").innerText = speedup.toFixed(2);
+document.getElementById("efficiency").innerText = efficiency.toFixed(2);
 
-data:{
-
-labels:["Serial","Parallel","Distributed"],
-
-datasets:[{
-
-label:"Execution Time (seconds)",
-
-data:[data.serial,data.parallel,data.distributed]
-
-}]
-
+drawCharts(data, speedup, efficiency);
 }
 
+function drawCharts(data, speedup, efficiency){
+
+if(mainChart) mainChart.destroy();
+if(metricChart) metricChart.destroy();
+
+mainChart = new Chart(document.getElementById("mainChart"), {
+type: "bar",
+data: {
+labels: ["Serial","Parallel","Distributed"],
+datasets: [{
+label: "Execution Time (s)",
+data: [data.serial, data.parallel, data.distributed]
+}]
+}
 });
 
+metricChart = new Chart(document.getElementById("metricChart"), {
+type: "line",
+data: {
+labels: ["Speedup","Efficiency"],
+datasets: [{
+label: "Performance",
+data: [speedup, efficiency]
+}]
+}
+});
 }
